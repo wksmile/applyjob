40bd86e93ac59fd4ea8192e3806dc9ddf864dbb0
wksmile <415893570@qq.com>
2018-01-04 09:24:37 +0800

    完成user部分

--- server/server.js
+++ server/server.js
@@ -1,42 +1,12 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const userRouter = require('./user')
 
-// 连接
-const DB_URL = 'mongodb://localhost:27017'
-
-mongoose.connect(DB_URL)
-mongoose.connection.on('connected',function () {
-    console.log('mongo connect success')
-})
-
-// 类似于mysql的表，mongodb里面有文档，字段的概念
-const User = mongoose.model('user', new mongoose.Schema({
-    user: {type: String, require:true},
-    age: {type:Number,require:true}
-}))
-// 新增数据
-User.create({
-    user:'imooc',
-    age:18
-},function (err, doc) {
-    if(!err) {
-        console.log(doc)
-    } else {
-        console.log(err)
-    }
-})
-
-const app = express();
-
-app.get('/',function (req, res) {
-    res.send('<h1>Hello world</h1>')
-})
-
-app.get('/data',function (req, res) {
-    User.find({},function (err, doc) {
-        res.json(doc)
-    })
-})
+const app = express()
+app.use(cookieParser())
+app.use(bodyParser.json())   // 解析post过来的json数据
+app.use('/user',userRouter)
 
 app.listen(9093,function () {
     console.log('Node app start at port 9093')
